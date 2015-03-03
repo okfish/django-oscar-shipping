6bff22c2552e78703a05c45fdb85dda4c0efcda1
Fish <okfish@yandex.ru>
2015-03-03 12:02:02 +0300

    Emspost facade skeleton.

--- oscar_shipping/facade/emspost.py
+++ oscar_shipping/facade/emspost.py
@@ -1,10 +1,44 @@
+import json
+
 from decimal import Decimal as D
 
-from ..exceptions import CityNotFoundError, ApiOfflineError
+from django.core.cache import cache
+from django.core.exceptions import ImproperlyConfigured
+from django.utils.translation import ugettext_lazy as _
+
+from emspost_api import emspost
+
+from ..exceptions import ( OriginCityNotFoundError, 
+                           CityNotFoundError, 
+                           ApiOfflineError, 
+                           TooManyFoundError,
+                           CalculationError )
 
 class ShippingFacade(object):
+    api = None
+    
     def __init__(self, api_user=None, api_key=None):
-        super(ShippingFacade, self).__init__()
+        self.api = emspost.EmsAPI()
+    
     def get_charges(self, weight, volume, origin, dest):
-        #TODO: get_charge method
-        raise ApiOfflineError()
+        if not self.api.is_online():
+            raise ApiOfflineError(_("Sorry. EMS API is offline right now"))
+        raise CalculationError("Not implemented yet. Sorry")
+
+    def get_extra_form(self, *args, **kwargs):
+        """
+        Return additional form if ambiguous data posted 
+        via shipping address form so calculate() method requires 
+        user action.
+        If no initial data present return simple calc form with origin predefined
+        If data given instantiate the choice form.   
+        """
+
+        # Return simple calculator form if no choices given: 
+        # assuming entered city not found in branches 
+        try:
+            from .forms import EmsCalcForm
+        except ImportError:
+            return None
+        return EmsCalcForm(*args, **kwargs)
+
